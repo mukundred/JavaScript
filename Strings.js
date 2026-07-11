92cf9429396c9200ca46f25701d6b19ce67ed4a9
mukundred <themukund23.official@gmail.com>
2026-07-11 21:05:07 +0530

    Strings

---
+++ Strings.js
@@ -0,0 +1,12 @@
+const name = "Mukund"
+
+const repo_count = 50
+
+// console.log(name + repo_count);
+
+// console.log(`My name is ${name} and repo_count is ${repo_count} `);
+
+const gameName = new String('God Of War')
+console.log(gameName[2]);
+console.log(gameName.toUpperCase());
+
