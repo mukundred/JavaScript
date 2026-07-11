11de7c1fa31685d59bf62c0355499441bee65f4e
mukundred <themukund23.official@gmail.com>
2026-07-11 21:36:12 +0530

    Numbers & Math

---
+++ numbers_maths.js
@@ -0,0 +1,29 @@
+const score = 400
+const balance = new Number(100)
+
+// console.log(balance);
+
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.89666
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+/// Maths
+console.log(Math.abs(-4));
+
+console.log(Math.round(-4.6754)); // ceil & floor
+
+
+console.log(Math.round((Math.random()*10) ));
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min ))
