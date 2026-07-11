5b957b8c2e98f346fb6215c56856e8a06a118196
mukundred <themukund23.official@gmail.com>
2026-07-11 21:46:19 +0530

    Date & Time

---
+++ dates.js
@@ -0,0 +1,14 @@
+// Dates
+let myDate = new Date()
+
+
+// console.log(myDate.toString());
+
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toDateString());
+
+
+let timeStamp = Date.now();
+
+console.log(Math.floor(Date.now()/1000));
