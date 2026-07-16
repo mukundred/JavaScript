32cde89c4f6302b5f438fde4e9eabb784a9596f4
mukundred <themukund23.official@gmail.com>
2026-07-16 22:10:52 +0530

    Array in JavaScript

---
+++ array.js
@@ -0,0 +1,61 @@
+// array
+
+const myArr = [0,1,2,3,4,5,6,7]
+constHeroes = ['superman', 'batman', 'shaktiman']
+// console.log(myArr[1]);
+
+//Array Methods
+
+// myArr.push(6)
+
+//myArr.pop();
+
+// myArr.unshift(9)
+// myArr.shift()
+
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(19));
+
+// const newArr = myArr.join()
+
+// console.log(myArr)
+// console.log(typeof newArr);
+
+
+// slice & splice
+
+// console.log("A ", myArr);
+
+// const myn1 = myArr.slice(1,3)
+
+// console.log(myn1);
+
+// console.log("B ", myArr);
+
+
+// const myn2 = myArr.splice(1,3)
+
+// console.log(myn2);
+
+// console.log("C ", myArr);
+
+// use concat to merge two arrays instead of push() 
+// as push is kind of create a different and make it tough to access 
+
+
+// or even better use spread = [...marvel heroes, ... dc_heroes]
+
+// const another_array = [1,2,3,[4,5,6,7], [1,2,3,4,[5454,23423,32]]]
+
+// const real_another_array = another_array.flat(Infinity)
+
+// console.log(real_another_array);
+
+
+let s1 =100;
+let s2 = 200;
+let s3 = 300;
+
+console.log(Array.of(s1,s2,s3));
+
